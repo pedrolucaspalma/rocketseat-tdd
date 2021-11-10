0a011114bea7aae8cf2ae76d0043e07cba97894f
pedrolucaspalma <pedrolucaspalma@gmail.com>
2021-11-10 15:38:55 -0300

    conclude writting first test. Add supertest

--- __tests__/integration/session.test.js
+++ __tests__/integration/session.test.js
@@ -1,12 +1,12 @@
-const {User} = require('../../src/app/models');
+const { User } = require('../../src/app/models');
 
 describe("Authentication", () => {
-  it("should should sum two numbers", () => {
-    const user = await User.create({name: 'Diego', email: 'diego@rocketseat', password_hash:"123123"})
-
-    console.log(user)
-
-    expect(user.email).toBe('diego@rocketseat.com.br')
-  });
+  it('should authenticate with valid credentials', async () =>{
+    const user = await User.create({
+      name: 'Diego',
+      email: 'diego@rocketseat.com.br',
+      password_hash: '123123'
+    })
+  })
 
 });
