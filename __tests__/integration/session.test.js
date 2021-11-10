8d9dad82f554641a160e4daa8320f2fe07746e83
pedrolucaspalma <pedrolucaspalma@gmail.com>
2021-11-10 13:53:38 -0300

    add env env.test. Add both to gitignore.

--- __tests__/integration/session.test.js
+++ __tests__/integration/session.test.js
@@ -1,3 +1,10 @@
-it('', () =>{
-    
-})
+describe("Authentication", () => {
+  it("should should sum two numbers", () => {
+    const x = 2;
+    const y = 4;
+
+    const sum = x + y
+
+    expect(sum).toBe(6);
+  });
+});
