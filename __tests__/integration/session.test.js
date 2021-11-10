5593eda52f7644b7e937a5d0a0837fa2219f32bc
pedrolucaspalma <pedrolucaspalma@gmail.com>
2021-11-10 19:45:22 -0300

    add 2 more tests

--- __tests__/integration/session.test.js
+++ __tests__/integration/session.test.js
@@ -1,28 +1,41 @@
 const request = require("supertest");
 
-const app = require('../../src/app')
+const app = require("../../src/app");
 const { User } = require("../../src/app/models");
-const truncate = require('../utils/truncate')
+const truncate = require("../utils/truncate");
 
 describe("Authentication", () => {
-  beforeAll(async ()=> {
+  beforeEach(async () => {
     await truncate();
-  })
+  });
 
   it("should authenticate with valid credentials", async () => {
     const user = await User.create({
       name: "Diego",
       email: "diego@rocketseat.com.br",
-      password_hash: "123123",
+      password: "123456",
     });
-    
-    const response = await request(app)
-    .post('/sessions')
-    .send({
-      email:user.email,
-      password: 123456
-    })
 
-    expect(response.status).toBe(200)
+    const response = await request(app).post("/sessions").send({
+      email: user.email,
+      password: "123456",
+    });
+
+    expect(response.status).toBe(200);
+  });
+
+  it("should not authenticate with invalid credentials", async () => {
+    const user = await User.create({
+      name: "Diego",
+      email: "diego@rocketseat.com.br",
+      password: "123123",
+    });
+
+    const response = await request(app).post("/sessions").send({
+      email: user.email,
+      password: "123456",
+    });
+
+    expect(response.status).toBe(401);
   });
 });
--- __tests__/unit/user.test.js
+++ __tests__/unit/user.test.js
@@ -16,7 +16,7 @@ describe("User", () => {
       password: "123456",
     });
 
-    const compareHash = await bcrypt.compare('123456', user.password_hash)
+    const compareHash = await bcrypt.compare("123456", user.password_hash);
 
     expect(compareHash).toBe(true);
   });
