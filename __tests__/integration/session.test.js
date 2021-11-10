266625acbf7d15e07cb8985f1516e54e5b1cc65a
pedrolucaspalma <pedrolucaspalma@gmail.com>
2021-11-10 15:53:18 -0300

    add SessionController.js, finish first test"

--- __tests__/integration/session.test.js
+++ __tests__/integration/session.test.js
@@ -1,12 +1,28 @@
-const { User } = require('../../src/app/models');
+const request = require("supertest");
+
+const app = require('../../src/app')
+const { User } = require("../../src/app/models");
+const truncate = require('../utils/truncate')
 
 describe("Authentication", () => {
-  it('should authenticate with valid credentials', async () =>{
-    const user = await User.create({
-      name: 'Diego',
-      email: 'diego@rocketseat.com.br',
-      password_hash: '123123'
-    })
+  beforeAll(async ()=> {
+    await truncate();
   })
 
+  it("should authenticate with valid credentials", async () => {
+    const user = await User.create({
+      name: "Diego",
+      email: "diego@rocketseat.com.br",
+      password_hash: "123123",
+    });
+    
+    const response = await request(app)
+    .post('/sessions')
+    .send({
+      email:user.email,
+      password: 123456
+    })
+
+    expect(response.status).toBe(200)
+  });
 });
--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,8 @@
 const routes = require("express").Router();
 
-
+const SessionController = require('./app/controllers/SessionController');
 // Definição rotas
+
+routes.post('/sessions', SessionController.store)
 
 module.exports = routes;
