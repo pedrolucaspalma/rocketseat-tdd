ce94784e0347acdf6a29227640bf06961a93aa9e
pedrolucaspalma <pedrolucaspalma@gmail.com>
2021-11-10 13:32:02 -0300

    create User Model. add nodemon. test user insertion

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,5 @@
 const routes = require("express").Router();
-// const { User } = require('./app/models');
 
-// User.create({name: 'Diego', email: 'diego@rocketseat.com.br', password_hash:'12314235'})
 
 // Definição rotas
 
