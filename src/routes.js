dd19f0c0773759eb0a2fc093d06d6ee761f044a7
pedrolucaspalma <pedrolucaspalma@gmail.com>
2021-11-10 13:20:24 -0300

    init

---
+++ src/routes.js
@@ -0,0 +1,8 @@
+const routes = require("express").Router();
+// const { User } = require('./app/models');
+
+// User.create({name: 'Diego', email: 'diego@rocketseat.com.br', password_hash:'12314235'})
+
+// Definição rotas
+
+module.exports = routes;
